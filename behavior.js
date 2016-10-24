470268244c489cf5c2b17e4af18d660b95a802c3
Olivier Lemelin <lemelin.olivier@gmail.com>
2016-10-24 16:44:47 -0400

    More changes.

--- behavior.js
+++ behavior.js
@@ -1,7 +1,52 @@
 (function () {
 
+  // Function to hide the main content sections of this page.
+  var hideContent = function(instantly) {
+    $('.content').hide(1);
+  };
+
+  var unhideElement = function(event) {
+    const activeElem = $( '.content' ).filter( ':visible' );
+    const clickedElem = $( '#' + _.split(event.currentTarget.id, '-')[0] );
+    const scrollToId = '#' + clickedElem[0].id + '-anchor';
+
+
+    // Scroll up the previously selected element, then hide it.
+    $(activeElem).slideUp(500).hide(1);
+
+    // Then, scroll down the new element.
+    $(clickedElem)
+      .delay(500)
+      .slideDown(500);
+
+    // We need this, otherwise there has been no time to refresh
+    // the layout of the site.
+    window.setTimeout(function() {
+
+      // Fetch the title element to scroll to.
+      var target = $(scrollToId);
+
+      // Animate the scroll to the new section.
+      $('html,body').animate({
+        scrollTop: target.offset().top
+      }, 500);
+
+    }, 1100);
+  };
+
   // On document ready, fade in the container.
   $(document).ready(function() {
+
+    // Make the sections all the same height.
+    $('.same-height').matchHeight();
+
+    // Bind the unhide function to the buttons.
+    $('.activator').on('click', unhideElement);
+
+    // Hide the content by default.
+    hideContent();
+
+    // Fade in the container once it has been properly set up.
     $(".container").hide(0).delay(500).fadeIn(1000);
   });
 
